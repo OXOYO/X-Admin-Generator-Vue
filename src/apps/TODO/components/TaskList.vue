0a58930d98dab66345e4a18e271124e24f195f62
OXOYO <zmn2007.hi@163.com>
2019-04-26 18:34:51 +0800

    完善TODO模块。

--- src/apps/TODO/components/TaskList.vue
+++ src/apps/TODO/components/TaskList.vue
@@ -8,8 +8,18 @@
   .task-box {
     .header {
       .title {
+        display: inline-block;
         font-size: 20px;
         font-weight: bold;
+        vertical-align: middle;
+      }
+      .tools {
+        display: inline-block;
+        margin: 0 10px;
+
+        .tool-item {
+          display: inline-block;
+        }
       }
     }
     .body {
@@ -22,15 +32,28 @@
           border-top: none;
           border-right: none;
           border-left: none;
+          color: inherit;
 
           &:hover,
           &:focus {
             box-shadow: none;
           }
         }
-
         .icon {
           cursor: pointer;
+        }
+      }
+    }
+    .list-block {
+      .input-block {
+        .ivu-input-prefix {
+          width: 170px;
+        }
+        .ivu-input-with-prefix {
+          padding-left: 170px;
+        }
+        .ivu-rate {
+          margin: 0 5px;
         }
       }
     }
@@ -38,14 +61,37 @@
 </style>
 
 <template>
-  <div class="task-box" v-if="Object.keys(currentMenu.info).length">
+  <div class="task-box">
     <div class="header">
-      <div class="title" :style="themeStyle">{{ currentMenu.info ? currentMenu.info.name : '' }}</div>
+      <div class="title" :style="themeStyle">{{ currentCategory.info ? currentCategory.info.name : '' }}</div>
+      <div class="tools">
+        <ColorPicker
+          v-if="currentCategory.info.create_user_id"
+          class="tool-item"
+          :transfer="false"
+          v-model="currentTheme"
+          @on-change="handleThemeChange"
+        />
+        <Dropdown
+          v-if="currentCategory.info.create_user_id"
+          class="tool-item"
+          trigger="click"
+          style="margin-left: 10px"
+          @on-click="handleAction"
+        >
+          <a href="javascript:void(0)">
+            <Icon type="ios-more" style="font-size: 20px;"/>
+          </a>
+          <DropdownMenu slot="list">
+            <DropdownItem name="edit">{{ $t('L00171') }}</DropdownItem>
+            <DropdownItem name="remove" style="color: #ed4014">{{ $t('L00124') }}</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </div>
     <div class="body">
       <div class="add-block">
         <Form
-          ref="addForm"
           :model="addForm"
           inline
           @submit.native.prevent
@@ -53,10 +99,12 @@
           <FormItem prop="content" style="width: 100%;">
             <Input
               class="input-block"
+              ref="addTask"
               v-model="addForm.content"
               :placeholder="$t('L00167')"
               size="large"
-              @on-enter.stop.prevent="doAdd"
+              :autofocus="true"
+              @on-enter.stop.prevent="doAddTask"
             >
               <Icon type="md-add" slot="prefix"/>
             </Input>
@@ -68,28 +116,38 @@
           inline
           @submit.native.prevent
         >
-          <FormItem
-            v-for="(item, index) in taskList"
-            :key="index"
-            style="width: 100%;"
+          <template
+            v-for="key in Object.keys(taskMap).sort().reverse()"
           >
-            <Input
-              class="input-block"
-              :value="item.content"
-              readonly=""
-              size="large"
+            <FormItem
+              v-for="(item, index) in taskMap[key]"
+              :key="item.index"
+              style="width: 100%;"
             >
-              <Icon
-                class="icon"
+              <Divider v-show="dividerMap[item.index]">
+                <Icon type="ios-arrow-down" />
+                {{ dividerMap[item.index] }}
+              </Divider>
+              <Input
+                class="input-block"
+                :value="'# ' + (index + 1) + ' ' + item.content"
+                readonly
+                size="large"
                 :style="themeStyle"
-                :type="item.icon"
-                slot="prefix"
-                @mouseover.native="handleOver(index)"
-                @mouseout.native="handleOut(index)"
-                @click.native="handleClick(item)"
-              />
-            </Input>
-          </FormItem>
+              >
+                <Icon
+                  class="icon"
+                  :style="themeStyle"
+                  :type="item.icon"
+                  slot="prefix"
+                  @mouseover.native="handleOver(item.index)"
+                  @mouseout.native="handleOut(item.index)"
+                  @click.native="handleStatusChange(item)"
+                />
+                <Rate slot="prefix" v-model="item.star" @on-change="(value) => handleStarChange(item, value)"/>
+              </Input>
+            </FormItem>
+          </template>
         </Form>
       </div>
     </div>
@@ -97,37 +155,34 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex'
-
   export default {
     name: 'TaskList',
     data () {
       return {
         addForm: {},
         taskList: [],
-        iconList: ['ios-radio-button-off', 'ios-checkmark-circle']
+        taskMap: {},
+        iconList: ['ios-radio-button-off', 'ios-checkmark-circle'],
+        currentCategory: {
+          type: '',
+          info: {}
+        },
+        currentTheme: '',
+        dividerMap: {}
       }
     },
     computed: {
-      ...mapState('Apps/TODO', {
-        currentMenu: state => state.currentMenu
-      }),
       themeStyle () {
         let _t = this
-        if (_t.currentMenu.info && _t.currentMenu.info.theme) {
+        if (_t.currentTheme) {
           return {
-            color: _t.currentMenu.info.theme
+            color: _t.currentTheme
           }
         }
         return {}
       }
     },
     methods: {
-      init () {
-        let _t = this
-        _t.initAddForm()
-        _t.getTaskList()
-      },
       initAddForm () {
         let _t = this
         _t.addForm = {
@@ -150,7 +205,7 @@
           _t.taskList[index].icon = 'ios-checkmark-circle-outline'
         }
       },
-      async handleClick (item) {
+      async handleStatusChange (item) {
         let _t = this
         let oldStatus = item.status
         // 分发action，调接口
@@ -164,17 +219,47 @@
         }
         // 处理返回数据
         _t.$Message.success(oldStatus ? _t.$t('L00168') : _t.$t('L00169'))
-        // 刷新列表
+        // 获取列表
         _t.getTaskList()
       },
-      async doAdd () {
+      async handleStarChange (item, value) {
+        let _t = this
+        // 分发action，调接口
+        let res = await _t.$store.dispatch('Apps/TODO/task/edit', {
+          id: item.id,
+          star: value
+        })
+        if (!res || res.code !== 200) {
+          return
+        }
+        // 处理返回数据
+        _t.$Message.success(res.msg)
+        // 获取列表
+        _t.getTaskList()
+      },
+      async handleThemeChange () {
+        let _t = this
+        // 分发action，调接口
+        let res = await _t.$store.dispatch('Apps/TODO/category/edit', {
+          id: _t.currentCategory.info.id,
+          theme: _t.currentTheme
+        })
+        if (!res || res.code !== 200) {
+          return
+        }
+        // 处理返回数据
+        _t.$Message.success(res.msg)
+        // 广播事件，刷新侧边栏
+        _t.$X.utils.bus.$emit('Apps/TODO/Sidebar/refresh')
+      },
+      async doAddTask () {
         let _t = this
         if (!_t.addForm.content) {
           return
         }
         let res = await _t.$store.dispatch('Apps/TODO/task/add', {
           content: _t.addForm.content,
-          category_id: _t.currentMenu.info.id
+          category_id: _t.currentCategory.info.id
         })
         if (!res || res.code !== 200) {
           return
@@ -183,14 +268,16 @@
         _t.$Message.success(res.msg)
         // 初始化表单
         _t.initAddForm()
-        // 刷新列表
+        // 获取列表
         _t.getTaskList()
       },
       // 获取任务列表
       async getTaskList () {
         let _t = this
         // 分发action，调接口
-        let res = await _t.$store.dispatch('Apps/TODO/task/list', {})
+        let res = await _t.$store.dispatch('Apps/TODO/task/list', {
+          category_id: _t.currentCategory.info.id
+        })
         if (!res || res.code !== 200) {
           return
         }
@@ -201,16 +288,87 @@
           _t.$Message.info(_t.$t('L00011'))
         }
         let resList = res.data.list || []
-        // 处理icon
+        let dividerMap = {}
+        let taskMap = {}
         for (let i = 0, len = resList.length; i < len; i++) {
+          // 存储索引
+          resList[i].index = i
+          // 处理icon
           resList[i].icon = _t.iconList[resList[i].status]
+          let currentItemDate = _t.$X.moment(resList[i].create_time).format('YYYY-MM-DD')
+          if (!Object.values(dividerMap).includes(currentItemDate)) {
+            dividerMap[i] = currentItemDate
+          }
+          if (!taskMap[currentItemDate]) {
+            taskMap[currentItemDate] = []
+          }
+          taskMap[currentItemDate].push(resList[i])
         }
         _t.taskList = resList
+        _t.taskMap = taskMap
+        _t.dividerMap = dividerMap
+      },
+      handleAction (name) {
+        let _t = this
+        switch (name) {
+          case 'edit':
+            _t.handleEditCategory()
+            break
+          case 'remove':
+            _t.handleRemoveCategory()
+            break
+        }
+      },
+      handleEditCategory () {
+        let _t = this
+        // 广播事件
+        _t.$X.utils.bus.$emit('Apps/TODO/Sidebar/category/edit', _t.currentCategory)
+      },
+      handleRemoveCategory () {
+        let _t = this
+        // 删除用户列表中所选用户
+        _t.$Modal.confirm({
+          title: _t.$t('L00141'),
+          content: _t.$t('L00060'),
+          onOk: async function () {
+            let res = await _t.$store.dispatch('Apps/TODO/category/remove', {
+              id: _t.currentCategory.info.id
+            })
+            if (!res || res.code !== 200) {
+              return
+            }
+            _t.$Message.info(res.msg)
+            // 广播事件，刷新侧边栏
+            _t.$X.utils.bus.$emit('Apps/TODO/Sidebar/refresh')
+          }
+        })
+      },
+      handleCategoryTrigger (data) {
+        let _t = this
+        _t.currentCategory = data
+        _t.currentTheme = data.info && data.info.theme ? data.info.theme : ''
+        // 初始化表单
+        _t.initAddForm()
+        // 获取列表
+        _t.getTaskList()
+        _t.$nextTick(function () {
+          if (_t.$refs.addTask && _t.$refs.addTask.autofocus) {
+            _t.$refs.addTask.focus()
+          }
+        })
       }
     },
     created () {
       let _t = this
-      _t.init()
+      // 监听事件
+      _t.$X.utils.bus.$on('Apps/TODO/Sidebar/category/trigger', function (data) {
+        _t.handleCategoryTrigger(data)
+      })
+    },
+    beforeDestroy: function () {
+      let _t = this
+      // 销毁监听
+      _t.$X.utils.bus.$off('Apps/TODO/Sidebar/category/trigger')
     }
   }
 </script>
--- src/apps/TODO/containers/Sidebar.vue
+++ src/apps/TODO/containers/Sidebar.vue
@@ -27,25 +27,64 @@
         <Icon v-if="item.icon" :type="item.icon"></Icon>
         <span>{{ item.lang ? $t(item.lang) : item.label}}</span>
       </MenuItem>
+      <!-- 分类菜单 系统默认 -->
       <MenuItem
         v-for="(item, index) in categoryMap.system"
         :key="'category_system_' + index"
         :name="'category_system_' + index"
       >
-        <Icon type="md-list" :style="{ color: item.theme }"></Icon>
+        <Icon :style="{ color: item.theme }" type="md-list"></Icon>
         <span :style="{ color: item.theme }">{{ item.lang ? $t(item.lang) : item.name}}</span>
       </MenuItem>
+      <!-- 分类菜单 用户自定义 -->
       <MenuItem
         v-for="(item, index) in categoryMap.custom"
         :key="'category_custom_' + index"
         :name="'category_custom_' + index"
       >
-        <Icon type="md-list"></Icon>
-        <span>{{ item.lang ? $t(item.lang) : item.name}}</span>
+        <Icon
+          v-show="!(currentCategory.type === 'category' && currentCategory.info && currentCategory.info.id === item.id)"
+          :style="{ color: item.theme }" type="md-list"
+        ></Icon>
+        <span
+          v-show="!(currentCategory.type === 'category' && currentCategory.info && currentCategory.info.id === item.id)"
+          :style="{ color: item.theme }"
+        >
+          {{ item.lang ? $t(item.lang) : item.name}}
+        </span>
+        <Input
+          v-show="currentCategory.type === 'category' && currentCategory.info && currentCategory.info.id === item.id"
+          :ref="'editCategory_' + item.id"
+          class="input-block"
+          v-model="editForm.name"
+          :placeholder="$t('L00170')"
+          size="large"
+          :autofocus="true"
+          @click.native.stop.prevent
+          @on-blur="handleBlur('edit')"
+          @on-enter.stop.prevent="doEditCategory"
+        >
+          <Icon type="md-add" slot="prefix"/>
+        </Input>
       </MenuItem>
-      <MenuItem name="addCategory">
-        <Icon type="md-add"></Icon>
-        <span>{{ $t('L00166')}}</span>
+      <!-- 新增分类 -->
+      <MenuItem name="action_addCategory">
+        <Icon v-show="!isAddingCategory" type="md-add"></Icon>
+        <span v-show="!isAddingCategory">{{ $t('L00166')}}</span>
+        <Input
+          v-show="isAddingCategory"
+          ref="addCategory"
+          class="input-block"
+          v-model="addForm.name"
+          :placeholder="$t('L00170')"
+          size="large"
+          :autofocus="true"
+          @click.native.stop.prevent
+          @on-blur="handleBlur('add')"
+          @on-enter.stop.prevent="doAddCategory"
+        >
+          <Icon type="md-add" slot="prefix"/>
+        </Input>
       </MenuItem>
     </Menu>
   </div>
@@ -63,21 +102,59 @@
           system: [],
           // 用户自定义分类
           custom: []
+        },
+        addForm: {
+          name: ''
+        },
+        editForm: {
+          id: '',
+          name: ''
+        },
+        // 是否正在添加分类
+        isAddingCategory: false,
+        currentActiveName: '',
+        currentCategory: {
+          type: '',
+          info: {}
         }
       }
     },
     computed: {
       ...mapState('Apps/TODO', {
-        menuList: state => state.menuList,
-        currentActiveName: state => state.currentActiveName
+        menuList: state => state.menuList
       })
     },
     methods: {
       async init () {
         let _t = this
+        _t.initAddForm()
+        _t.initEditForm()
+        _t.initCurrentCategory()
         await _t.getCategoryList()
-        // 触发菜单
-        _t.triggerMenu('category_system_0')
+        // 处理激活菜单
+        _t.handleActiveName()
+        // 触发菜单激活
+        _t.triggerMenu(_t.currentActiveName)
+      },
+      initAddForm () {
+        let _t = this
+        _t.addForm = {
+          name: ''
+        }
+      },
+      initEditForm (data) {
+        let _t = this
+        _t.editForm = data || {
+          id: '',
+          name: ''
+        }
+      },
+      initCurrentCategory () {
+        let _t = this
+        _t.currentCategory = {
+          type: '',
+            info: {}
+        }
       },
       // 获取分类列表
       async getCategoryList () {
@@ -113,33 +190,134 @@
         // 分发mutation，更新state
         _t.$store.commit('Apps/TODO/categoryMap/update', categoryMap)
       },
+      // 处理激活菜单
+      handleActiveName () {
+        let _t = this
+        if (_t.menuList.filter(item => item.enable).length) {
+          _t.currentActiveName = 'menu_0'
+        } else if (_t.categoryMap.system.length) {
+          _t.currentActiveName = 'category_system_0'
+        } else if (_t.categoryMap.custom.length) {
+          _t.currentActiveName = 'category_custom_0'
+        }
+      },
       triggerMenu (name) {
         let _t = this
         let currentMenu = {
           type: '',
           info: {}
         }
-        let parent
-        if (name.includes('menu')) {
-          currentMenu.type = 'menu'
-        } else if (name.includes('category')) {
-          currentMenu.type = 'category'
-          parent = name.split('_')[1]
-        }
-        let index = name.split('_').pop()
-        if (currentMenu.type === 'menu') {
-          currentMenu.info = _t.menuList[index]
-        } else if (currentMenu.type === 'category') {
-          currentMenu.info = _t.categoryMap[parent][index]
+        if (name.includes('menu') || name.includes('category')) {
+          let parent
+          if (name.includes('menu')) {
+            currentMenu.type = 'menu'
+          } else if (name.includes('category')) {
+            currentMenu.type = 'category'
+            parent = name.split('_')[1]
+          }
+          let index = name.split('_').pop()
+          if (currentMenu.type === 'menu') {
+            currentMenu.info = _t.menuList[index]
+          } else if (currentMenu.type === 'category') {
+            currentMenu.info = _t.categoryMap[parent][index]
+          }
+        } else if (name.includes('action')) {
+          let actionName = name.split('_')[1]
+          currentMenu = {
+            type: 'action',
+            info: {
+              name: actionName
+            }
+          }
+          if (actionName === 'addCategory') {
+            _t.isAddingCategory = true
+            _t.$nextTick(function () {
+              if (_t.$refs.addCategory && _t.$refs.addCategory.autofocus) {
+                _t.$refs.addCategory.focus()
+              }
+            })
+          }
         }
         // 分发mutation，更新state
-        _t.$store.commit('Apps/TODO/currentActiveName/update', name)
         _t.$store.commit('Apps/TODO/currentMenu/update', currentMenu)
+        // 处理分类激活
+        if (currentMenu.type === 'category') {
+          _t.$nextTick(function () {
+            // 广播事件
+            _t.$X.utils.bus.$emit('Apps/TODO/Sidebar/category/trigger', currentMenu)
+          })
+        }
+      },
+      async doAddCategory () {
+        let _t = this
+        if (!_t.addForm.name) {
+          return
+        }
+        let res = await _t.$store.dispatch('Apps/TODO/category/add', _t.addForm)
+        if (!res || res.code !== 200) {
+          return
+        }
+        // 处理返回数据
+        _t.$Message.success(res.msg)
+        // 初始化表单
+        _t.initAddForm()
+        // 刷新列表
+        _t.getCategoryList()
+      },
+      async doEditCategory () {
+        let _t = this
+        if (!_t.editForm.name) {
+          return
+        }
+        let res = await _t.$store.dispatch('Apps/TODO/category/edit', _t.editForm)
+        if (!res || res.code !== 200) {
+          return
+        }
+        // 处理返回数据
+        _t.$Message.success(res.msg)
+        // 初始化表单
+        _t.initEditForm()
+        _t.initCurrentCategory()
+        // 刷新列表
+        _t.getCategoryList()
+      },
+      handleBlur (action) {
+        let _t = this
+        switch (action) {
+          case 'add':
+            _t.isAddingCategory = false
+            _t.initAddForm()
+            break
+          case 'edit':
+            _t.initEditForm()
+            _t.initCurrentCategory()
+            break
+        }
       }
     },
     created () {
       let _t = this
       _t.init()
+      _t.$X.utils.bus.$on('Apps/TODO/Sidebar/refresh', function () {
+        // 刷新列表
+        _t.getCategoryList()
+      })
+      _t.$X.utils.bus.$on('Apps/TODO/Sidebar/category/edit', function (data) {
+        _t.currentCategory = data
+        _t.initEditForm(data.info)
+        _t.$nextTick(function () {
+          let key = 'editCategory_' + data.info.id
+          if (_t.$refs[key][0] && _t.$refs[key][0].autofocus) {
+            _t.$refs[key][0].focus()
+          }
+        })
+      })
+    },
+    beforeDestroy: function () {
+      let _t = this
+      // 销毁监听
+      _t.$X.utils.bus.$off('Apps/TODO/Sidebar/refresh')
+      _t.$X.utils.bus.$off('Apps/TODO/Sidebar/category/edit')
     }
   }
 </script>
